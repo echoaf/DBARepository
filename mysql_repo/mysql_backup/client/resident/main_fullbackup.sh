4e6728b4e618a7e5101db7138b4ac68cc267f1bf
root <root@172.16.112.10>
2018-12-05 22:26:08 +0800

    opt mydumper.sh

--- mysql_repo/mysql_backup/client/resident/main_fullbackup.sh
+++ mysql_repo/mysql_backup/client/resident/main_fullbackup.sh
@@ -4,64 +4,39 @@
 # arthur
 # 2018-10-16
 
-
 base_dir="/data/repository/mysql_repo/mysql_backup"
 common_dir="$base_dir/common"
 shell_cnf="$common_dir/shell.cnf"
 source $shell_cnf
-
 f_name=$(basename "$0")
 f_lock="$tmp_dir/"$(basename "$0"| awk -F"." '{print $1}')".sock"
-
 bd=$(date +%s) # 脚本开始执行时间
 maxr_second=3600 # 脚本执行时间
 
 
-function backupFunctionMain()
+
+function syncFullResult()
 {
-    backup_mode="$1"
-    curday=$(date +"%F")
-    tmp_type=$(echo "$type_name"| tr 'a-z' 'A-Z')
-    tmp_time=$(echo "$task_id"| awk -F"_" '{print $2}')
-    backup_path="${full_backup_dir}/${tmp_type}/${tmp_time}" # 备份目录
-    mkdir -p $backup_path
-
-    # Tips:依赖mydumper_backup脚本返回值
-    tmp_bmode=$(echo "$backup_mode"| tr 'a-z' 'A-Z')
-    if [ "$tmp_bmode" = "MYDUMPER" ];then
-        mydumperBackup $master_host $master_port $backup_path "N" $normal_log
-    elif [ "$tmp_bmode" = "XTRABACKUP" ];then
-        master_my_cnf="/data/mysql/$master_port/my.cnf"
-        xtrabackupBackup $master_host $master_port 22 $master_my_cnf $local_ip 22 $backup_path "N" "$normal_log"
-    else
-        printLog "[$f_name]未知的备份方式$backup_mode" "$normal_log"
-        ec >/dev/null 2>&1 # Tips:生成一个错误
-    fi
-
-    if (($?==0));then
-        printLog "[$f_name][$task_id $type_name $master_host:$master_port] 尝试备份Succ" "$normal_log"
-        tmp_bstatus="Backing"
-        tmp_memo="尝试备份成功"
-    else
-        printLog "[$f_name][$task_id $type_name $master_host:$master_port] 尝试备份Fail" "$normal_log"
-        tmp_bstatus="Fail"
-        tmp_memo="尝试备份失败"
-        # 备份失败Nice值自增1
-        echo "update $t_mysql_fullbackup_info set Fnice=Fnice+1,Fmodify_time=now() where Ftype='$type_name' and Fstate='online';" | $DBA_MYSQL
-    fi
-
+    task_id="$1"
+    instance="$2"
+    master_host="$3"
+    master_port="$4"
+    backup_mode="$5"
+    backup_path="$6"
+    back_status="$7"
+    backup_info="$8"
     is_task_id=$(echo "select count(*) from $t_mysql_fullbackup_result where Ftask_id='$task_id'"| $DBA_MYSQL)
     if ((${is_task_id}==0));then
         sql="insert into $t_mysql_fullbackup_result 
         (Ftype,Ftask_id,Fdate_time,Fdata_source,Fbackup_mode,
         Fbackup_address,Fbackup_path,Fback_status,Fbackup_info,Fcreate_time,Fmodify_time) 
         values 
-        ('$type_name','$task_id','$curday','$master_host:$master_port','$backup_mode',
-        '$local_ip','$backup_path','$tmp_bstatus','$tmp_memo',now(),now());"
+        ('$instance','$task_id',CURDATE(),'$master_host:$master_port','$backup_mode',
+        '$local_ip','$backup_path','$back_status','$backup_info',now(),now());"
     else
         sql="update $t_mysql_fullbackup_result 
-        set Ftype='$type_name',Fdate_time='$curday',Fdata_source='$master_host:$master_port',Fbackup_mode='$backup_mode',
-        Fbackup_address='$local_ip',Fbackup_path='$backup_path',Fback_status='$tmp_bstatus',Fbackup_info='$tmp_memo',
+        set Ftype='$instance',Fdate_time=CURDATE(),Fdata_source='$master_host:$master_port',Fbackup_mode='$backup_mode',
+        Fbackup_address='$local_ip',Fbackup_path='$backup_path',Fback_status='$back_status',Fbackup_info='$backup_info',
         Fmodify_time=now()
         where Ftask_id='$task_id';"
     fi
@@ -69,40 +44,109 @@ function backupFunctionMain()
 }
 
 
+function backupFunctionMain()
+{
+
+    task_id="$1"
+    instance="$2"
+    backup_mode="$3"
+    master_host="$4"
+    master_port="$5"
+    
+    suff_time=$(echo "$task_id"| awk -F"_" '{print $NF}')
+    backup_path="$full_backup_dir/$(upperString "$instance")/${suff_time}" # 备份目录
+    mkdir -p $backup_path
+
+    # Tips:依赖mydumper_backup脚本返回值
+    tmp_bmode=$(upperString "$backup_mode")
+    if [ "$tmp_bmode" = "MYDUMPER" ];then
+        return_info=$(checkMydumper "$master_host" "$master_port" "$backup_path")
+        if (($?==0));then
+            return_info=$(backupMydump "$master_host" "$master_port" "$backup_path" "N")
+        else
+            ec >/dev/null 2>&1 # Tips:主动生成一个错误
+        fi
+    elif [ "$tmp_bmode" = "XTRABACKUP" ];then
+        echo 2
+        master_my_cnf="/data/mysql/$master_port/my.cnf"
+        return_info=$(xtrabackupBackup $master_host $master_port 22 $master_my_cnf $local_ip 22 $backup_path "N" "$normal_log")
+    elif [ "$tmp_bmode" = "MYSQLDUMP" ];then
+        echo 3
+        #return_info=$(xtrabackupBackup $master_host $master_port 22 $master_my_cnf $local_ip 22 $backup_path "N" "$normal_log")
+    else
+        return_info="[$f_name][$task_id]unknow backup mode($backup_mode)"
+        ec >/dev/null 2>&1 # Tips:主动生成一个错误
+    fi
+
+    if (($?==0));then
+        printLog "[$f_name][$task_id]$return_info" "$normal_log"
+        tmp_bstatus="Backing"
+        tmp_memo="尝试备份成功"
+    else
+        printLog "[$f_name][$task_id]$return_info" "$normal_log"
+        tmp_bstatus="Fail"
+        tmp_memo="尝试备份失败:$return_info"
+    fi
+    syncFullResult "$task_id" "$instance" "$master_host" "$master_port" "$backup_mode" "$backup_path" "$tmp_bstatus" "$tmp_memo" 
+}
+
+
+
+function getTaskID()
+{
+    instance="$1"
+    weekday="$2"
+    # Tips:task_id生成规则
+    # 每周备份一次:instance_备份日期
+    # 每天备份:instance_当天日期
+    if ((${weekday}!=9));then
+        task_id="${instance}_$(date -d "$(($(date +%w)-${weekday})) days ago" +"%Y%m%d")"
+    else
+        task_id="${instance}_$(date +"%Y%m%d")"
+    fi
+    echo "$task_id"
+}
+
+
+function checkBackupTime()
+{
+    start_time="$1"
+    end_time="$2"
+    cur_time="$3"
+    start_pos=$(date -d "$start_time" +"%s") # 每天开始启动备份时间点(unix时间戳,下同)
+    end_pos=$(date -d "$end_time" +"%s") # 备份结束时间点
+    cur_pos=$(date -d "$cur_time" +"%s") # 当前时间点
+
+    if [ -z "$start_pos" ];then
+        start_pos=$(date -d "00:00:01" +"%s")
+    fi
+    if [ -z "$end_pos" ];then
+        end_pos=$(date -d "08:00:00" +"%s")
+    fi
+    if ((${cur_pos}>=${start_pos})) && ((${cur_pos}<=${end_pos}));then
+        E="0"
+    else
+        E="1"
+    fi
+    return "$E"
+}
+
+
 function backupChild()
 {
     indexs="$1"
-    echo "$indexs"
-    exit
     for index in $(echo "$indexs")
     do
-        type_name=$(echo "select Ftype from $t_mysql_fullbackup_info where Findex='$index';"| $DBA_MYSQL)
+        instance=$(echo "select Ftype from $t_mysql_fullbackup_info where Findex='$index';"| $DBA_MYSQL)
         weekday=$(echo "select Fbackup_weekday from $t_mysql_fullbackup_info where Findex='$index';"| $DBA_MYSQL)
-        # Tips:task_id生成规则
-        # 每周备份一次:type_name_备份日期
-        # 每天备份:type_name_当天日期
-        if ((${weekday}!=9));then
-            task_id="${type_name}_$(date -d "$(($(date +%w)-${weekday})) days ago" +"%Y%m%d")"
-        else
-            task_id="${type_name}_$(date +"%Y%m%d")"
-        fi
-
-        # Tips:version2.0
+        task_id=$(getTaskID "$instance" "$weekday")
         start_time=$(echo "select Fstart_time from $t_mysql_fullbackup_info where Findex='$index';"| $DBA_MYSQL)
         end_time=$(echo "select Fend_time from $t_mysql_fullbackup_info where Findex='$index';"| $DBA_MYSQL)
         cur_time=$(date +"%H:%M:%S")
-        start_pos=$(date -d "$start_time" +"%s") # 每天开始启动备份时间点(unix时间戳,下同)
-        end_pos=$(date -d "$end_time" +"%s") # 备份结束时间点
-        cur_pos=$(date -d "$cur_time" +"%s") # 当前时间点
-        if [ -z "$start_pos" ];then
-            start_pos=$(date -d "00:00:01" +"%s")
-        fi
-        if [ -z "$end_pos" ];then
-            end_pos=$(date -d "08:00:00" +"%s")
-        fi
-
-        if ((${cur_pos}>=${start_pos})) && ((${cur_pos}<=${end_pos})); then
-            printLog "[$f_name][$task_id]当前事件点${cur_time}在备份时间点(${start_time}-${end_time})内,开始进入今天的备份逻辑"
+ 
+        checkBackupTime "$start_time" "$end_time" "$cur_time"   
+        if (($?==0));then
+            printLog "[$f_name][$task_id]current time is ${cur_time}, enter backup logic(start_time:$start_time,end_time:$end_time)." "$normal_log"
             data_source=$(echo "select Fdata_source from $t_mysql_fullbackup_info where Findex='$index';"| $DBA_MYSQL)
             backup_mode=$(echo "select Fbackup_mode from $t_mysql_fullbackup_info where Findex='$index';"| $DBA_MYSQL)
             master_host=$(echo "$data_source"| awk -F":" '{print $1}')
@@ -110,15 +154,18 @@ function backupChild()
 
             # Tips:Succ和Backing状态的task_id不进入逻辑
             back_status=$(echo "select Fback_status from $t_mysql_fullbackup_result where Ftask_id='$task_id'"| $DBA_MYSQL)
-            tmp_bstatus=$(echo "$back_status"|tr 'a-z' 'A-Z')
+            tmp_bstatus=$(upperString "$back_status")
+            if [ -z "$back_status" ];then
+                back_status="initbackup"
+            fi
             if [ "$tmp_bstatus" = "SUCC" ] || [ "$tmp_bstatus" = "BACKING" ];then
-                printLog "[$f_name][$task_id]当前备份状态为:${back_status},不进入备份逻辑" "$normal_log"
+                printLog "[$f_name][$task_id]backup status is ${back_status},return" "$normal_log"
             else
-                printLog "[$f_name][$task_id]当前备份状态为:${back_status},进入备份逻辑" "$normal_log"
-                backupFunctionMain "$backup_mode"
+                printLog "[$f_name][$task_id]backup status is ${back_status},enter continue" "$normal_log"
+                backupFunctionMain "$task_id" "$instance" "$backup_mode" "$master_host" "$master_port"
             fi
         else
-            printLog "[$f_name][$task_id]当前事件点${cur_time}不在备份时间点(${start_time}-${end_time})内,不进入今天的备份逻辑"
+            printLog "[$f_name][$task_id]current time is ${cur_time}, don't enter backup logic(start_time:$start_time,end_time:$end_time)." "$normal_log"
         fi
     done
 }
@@ -131,6 +178,7 @@ function backupMain()
     # 并且没有在备份成功或正在备份中的数据库
 
     # 今天之前需要备份，但是没有在备份成功或者备份中的数据库
+    printLog "[$f_name]start deal with ervery week backupInfo." "$normal_log" 
     sql="select Findex from $t_mysql_fullbackup_info 
     where Fstate='online' and Fbackup_address='$local_ip'
     and Fbackup_weekday<='$curweek'
@@ -139,8 +187,10 @@ function backupMain()
     where Fdate_time>='$monday' and Fdate_time<='$sunday' and Fback_status in ('Succ','Backing'));"
     indexs=$($DBA_MYSQL -e "$sql")
     backupChild "$indexs"
+    printLog "[$f_name]end deal with ervery week backupInfo." "$normal_log" 
 
     # 每天需要备份，但是没有在备份中的数据库
+    printLog "[$f_name]start deal with ervery day backupInfo." "$normal_log" 
     sql="select Findex from $t_mysql_fullbackup_info 
     where Fstate='online' and Fbackup_address='$local_ip'
     and Fbackup_weekday=9
@@ -149,12 +199,12 @@ function backupMain()
     where Fdate_time>='$monday' and Fdate_time<='$sunday' and Fback_status in ('Backing'));"
     indexs=$($DBA_MYSQL -e "$sql")
     backupChild "$indexs"
+    printLog "[$f_name]end deal with ervery day backupInfo." "$normal_log" 
 }
 
 
 function checkBackStatus()
 {
-    # 此indexs非彼indexs
     indexs=$(echo "select Findex from $t_mysql_fullbackup_result 
     where Fdate_time>='$monday' and Fdate_time<='$sunday' 
     and Fback_status='Backing' and Fbackup_address='$local_ip';"| $DBA_MYSQL)
@@ -166,36 +216,49 @@ function checkBackStatus()
         backup_mode=$(echo "select Fbackup_mode from $t_mysql_fullbackup_result where Findex='$index';"| $DBA_MYSQL)
         data_source=$(echo "select Fdata_source from $t_mysql_fullbackup_result where Findex='$index';"| $DBA_MYSQL)
 
-        tmp_bmode=$(echo "$backup_mode"| tr 'a-z' 'A-Z')
+        tmp_bmode=$(upperString $backup_mode)
+
         if [ "$tmp_bmode" = "MYDUMPER" ];then
-            mydumperResultCheck ""$backup_path/metadata""
+            metadata="$backup_path/metadata"
+            json_info=$(checkMydumperResult "$metadata" "$data_source")
         elif [ "$tmp_bmode" = "XTRABACKUP" ];then
-            # 写死了backup.tar
-            xtrabackupResultCheck "$backup_path/backup.tar"
+            json_info=$(xtrabackupResultCheck "$backup_path")
+        elif [ "$tmp_bmode" = "MYSQLDUMP" ];then
+            return_info="hell mysqldump"
         else
-            return_info="备份失败,未知的备份方式"
-            ech >/dev/null 2>&1 # Tips:生成一个失败
+            return_info="unknow backup mode."
+            ec >/dev/null 2>&1 # Tips:生成一个失败
         fi
 
-        if (($?==0));then
-            echo "update $t_mysql_fullbackup_result 
-            set Fbackup_size='$size',Fbackup_start_time='$f_start_time',Fbackup_end_time='$f_end_time',
-            Fbackup_metadata=\"$metadata_jason\",Fback_status='Succ',Fmodify_time=now(),Fbackup_info='$return_info'
-            where Ftask_id='$task_id';" | $DBA_MYSQL
-            # 刷新nice值为0
-            echo "update $t_mysql_fullbackup_info set Fnice=0,Fmodify_time=now() 
-            where Ftype='$type_name' and Fstate='online';" | $DBA_MYSQL
-        elif (($?==1));then
-            echo "update $t_mysql_fullbackup_result 
-            set Fback_status='Backing',Fmodify_time=now(),Fbackup_info='$return_info' 
-            where Ftask_id='$task_id';" | $DBA_MYSQL
+        value="$?"
+        if (($value==0));then
+            backup_start_time=$(echo "$json_info"| awk -F"--" '{print $1}')
+            backup_end_time=$(echo "$json_info"| awk -F"--" '{print $2}')
+            backup_size=$(echo "$json_info"| awk -F"--" '{print $3}')
+            metadata_jason=$(echo "$json_info"| awk -F"--" '{print $4}')
+            back_status="Succ"
+            # Tips:归档留给check脚本,仅仅验证数据是有效集之后再进行压缩
+            #dir_path=$(dirname "$backup_path")
+            #base_path=$(basename "$backup_path")
+            #archivePath "$dir_path" "$base_path" # 备份数据压缩归档
+            #archive_file="${dir_path}/${base_path}.tar.gz"
+            sql="update $t_mysql_fullbackup_result 
+            set Fbackup_size='$backup_size',Fbackup_start_time='$backup_start_time',Fbackup_end_time='$backup_end_time',
+            Fbackup_metadata=\"$metadata_jason\",Fback_status='$back_status',Fmodify_time=now(),Fbackup_info='Succ'
+            where Ftask_id='$task_id';"
+        elif (($value==1));then
+            back_status="Backing"
+            sql="update $t_mysql_fullbackup_result 
+            set Fback_status='Backing',Fmodify_time=now(),Fbackup_info='$json_info' 
+            where Ftask_id='$task_id';"
         else
-            echo "update $t_mysql_fullbackup_result 
-            set Fback_status='Fail',Fmodify_time=now(),Fbackup_info='$return_info'
-            where Ftask_id='$task_id';" | $DBA_MYSQL
-            echo "update $t_mysql_fullbackup_info set Fnice=Fnice+1,Fmodify_time=now() 
-            where Ftype='$type_name' and Fstate='online';" | $DBA_MYSQL
+            back_status="Fail"
+            sql="update $t_mysql_fullbackup_result 
+            set Fback_status='$back_status',Fmodify_time=now(),Fbackup_info='$json_info'
+            where Ftask_id='$task_id';"
         fi
+        printLog "[$task_id] backup is $back_status($json_info)"
+        echo "$sql" | $DBA_MYSQL
     done
 }
 
@@ -206,7 +269,6 @@ function main()
 
     while ((1))
     do
-        curday=$(date +"%F")
         curweek=$(date +"%w") 
         sunday="$(date -d "$(($(date +%w)-6)) days ago" +"%Y-%m-%d")" # 本周周六日期
         monday="$(date -d "$(($(date +%w)-0)) days ago" +"%Y-%m-%d")" # 本周周日日期
@@ -214,14 +276,15 @@ function main()
         printLog "[$f_name]start full backup do,please don't quit at will." "$normal_log" 
         backupMain 
         printLog "[$f_name]end full backup do,quit if you need." "$normal_log" 
-        sleep 60 # 开始备份后不一定马上会生成备份文件,需要sleep
-
+        #sleep 60 # 开始备份后不一定马上会生成备份文件,需要sleep
         # 检测备份成功失败状态(只检测Backing状态的task_id)
         printLog "[$f_name]start full backup check." "$normal_log" 
-        #checkBackStatus
-
+        checkBackStatus
+        exit
+        printLog "[$f_name]end full backup check." "$normal_log" 
         sleep 10 # 留给退出时间
-        lastExit $bd $maxr_second
+
+        lastExit $bd $maxr_second $f_name
     done
 }
 
--- test.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# coding=utf8
-# 2018-10-28 
-# 2018-11-01
